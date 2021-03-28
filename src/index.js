87e5bf068b17ba3bf43e73dade042dcba2aaadcc
Lucas Borges <45474687+lucasborgesdev@users.noreply.github.com>
2021-03-27 21:48:27 -0300

    Pé na tabua

--- src/index.js
+++ src/index.js
@@ -1,21 +1,38 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
   // Complete aqui
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const { name,username } = request.body;
+
+  const userAlreadyExists = users.some(
+    (user) => user.name === name
+  );
+
+  if(userAlreadyExists) {
+    return response.status(400).json({ error: "User already Exists! "});
+  }
+
+
+  users.push({
+    id: uuidv4(),
+    user,
+    username,
+    todos: [] 
+  });
+
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
